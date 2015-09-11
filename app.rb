451d61d5c6c5710e66f4aec0347cb43bd675d686
Emily Carson <emilyjanecarson@gmail.com>
2015-09-11 11:55:48 +0100

    added acces token as env variable

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 
 # set :public_folder, 'public'
 
+APIKEY = ENV['githubToken']
+
 get '/' do
   send_file 'public/index.html'
+end
+
+get '/key' do
+  { access_token: APIKEY }.to_json
 end
--- public/js/searchFactory.js
+++ public/js/searchFactory.js
@@ -1,12 +1,20 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
+
   var queryUrl = 'https://api.github.com/search/users';
+
+  var accessToken = $http( {method: 'GET', url: '/key'} );
+
+  accessToken.then(function(results) {
+    self.token = results.data;
+  });
+
   return {
     query: function(searchTerm) {
       return $http({
         url: queryUrl,
         method: 'GET',
         params: {
-          'q': searchTerm, access_token: githubToken
+          'q': searchTerm, access_token: self.token.access_token
         }
       }); 
     }
